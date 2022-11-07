22c555db11591d40d737bb1e78d166914d73e20d
Maksim Goltsov <mgoltsov@bk.ru>
2022-11-07 12:12:34 +0300

    DAO inserted

--- src/main/java/ru/alishev/springcourse/DAO/PersonDAO.java
+++ src/main/java/ru/alishev/springcourse/DAO/PersonDAO.java
@@ -1,4 +1,29 @@
 package ru.alishev.springcourse.DAO;
 
+import ru.alishev.springcourse.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Aik"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Charles"));
+        people.add(new Person(++PEOPLE_COUNT, "Dude "));
+    }
+
+    public List<Person> index(){
+        return people;
+    }
+    public Person show(int id){
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+
+
 }
